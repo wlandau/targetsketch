8fd330570d32ee42597d9767ff3946eb5ffbae65
parmsam <parmartsam@gmail.com>
2022-10-10 21:08:21 -0500

    update script param documentation and move param checks to api.R

--- R/api.R
+++ R/api.R
@@ -1,12 +1,20 @@
 #' @title Launch the targetsketch app
 #' @description Launch an interactive web application for
 #'   creating and visualizing `targets` pipelines.
-#' @param script path of reference _targets.R script
+#' @param script Character of length 1, path to an existing targets script.
+#' When `script` is `NULL` (default) then the function uses the default
+#' starting script. If `script` file does not exist then function will stop
+#' and user will get an error message.
 #' @export
 #' @examples
 #' \dontrun{
 #' targetsketch()
 #' }
 targetsketch <- function(script = NULL) {
-  shiny::shinyApp(ui = ui(path = script), server = server)
+  if (file.exists(script) || is.null(script)) {
+    shiny::shinyApp(ui = ui(script = script), server = server)
+  } else {
+    stop("Targets script does not exist at specified path. 
+          Please specify a file path that exists.")
+  }
 }
